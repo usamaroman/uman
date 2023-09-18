7ee49e9e6ed0da83a45a8254fee29128eed6fc94
Banana-Duck <a.usama@yandex.ru>
2023-09-18 13:14:23 +0300

    repl created #7

--- cmd/main.go
+++ cmd/main.go
@@ -1,31 +1,9 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
 	"log"
-	"os"
-	"uman/repl"
 )
 
 func main() {
 	log.Println("Uman started")
-
-	args := os.Args
-	switch len(args) {
-	case 1:
-		fmt.Println("Welcome to the Uman progrmming language repl!")
-		repl.Start(os.Stdin)
-	case 2:
-		file, err := os.Open(args[1])
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		text := bufio.NewReader(file)
-		repl.Start(text)
-	default:
-		log.Fatal("wrong argument")
-	}
-
 }
